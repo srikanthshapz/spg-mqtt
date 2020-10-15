a569f8f459f1d7e25c8f03a9dae56571cf1cf3e0
H270076 <srikanth.shapursridharmurthy@honeywell.com>
2020-10-14 20:24:18 -0400

    updated build

--- build/lib/src/common/config.py
+++ build/lib/src/common/config.py
@@ -2,6 +2,6 @@ class Config:
     spgproto            =        "spBv1.0"
     groupId              =       "HBT Security" 
     brokerAddr            =       "localhost"
-    brokerPort           =       1883
+    brokerPort           =       1886
     myUsername           =       "admin"
     myPassword           =       "changeme"
